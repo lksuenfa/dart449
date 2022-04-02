c37be4165f541c2607c6278e8690cb6f230afa04
lksuenfa <70963322+lksuenfa@users.noreply.github.com>
2022-04-01 20:10:16 -0400

    final :added project

---
+++ project/final/js/horizontalScroll.js
@@ -0,0 +1,50 @@
+// Source: Santo Romano - DART449 class 6 files :horizontal scroll
+
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = gsap.utils.toArray("section");
+const triggerAnimation = gsap.utils.toArray(".animate");
+
+let maxWidth = 0;
+
+// find width of each section
+const getMaxWidth = () => {
+  maxWidth = 0;
+
+  sections.forEach((section) => {
+    maxWidth += section.offsetWidth;
+  });
+};
+getMaxWidth();
+
+// activate horizontal scroll only if screen size bigger than 700px
+ScrollTrigger.matchMedia({
+  "(min-width:600px)": function () {
+    ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+    gsap.to(sections, {
+      x: () => `-${maxWidth - window.innerWidth}`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".main-content",
+        pin: true,
+        scrub: true,
+        end: () => `+=${maxWidth}`,
+        invalidateOnRefresh: true,
+      },
+    });
+
+    triggerAnimation.forEach((sct, i) => {
+      ScrollTrigger.create({
+        trigger: sct,
+        start: () =>
+          "top top-=" +
+          (sct.offsetLeft - window.innerWidth / 4) *
+            (maxWidth / (maxWidth - window.innerWidth)),
+        end: () =>
+          "+=" + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
+        toggleClass: { targets: sct, className: "active" },
+      });
+    });
+  },
+});
